228e93472806207ff951696a4d8d2141bc8e26b7
Manpreet singh <71442397+manpreetsingh153@users.noreply.github.com>
2024-08-01 13:29:14 +0530

    Update route.js

--- src/app/api/detail/route.js
+++ src/app/api/detail/route.js
@@ -3,12 +3,11 @@ import { PrismaClient } from "@prisma/client";
 var mysql = require("mysql");
  
 import { NextRequest, NextResponse } from "next/server";
-
+const prisma = new PrismaClient()
 export async function GET() {
 
   try {
-         const user = await prisma.user.findMany({ })
-
+      const user = await prisma.user.findMany({})
      return NextResponse.json({data: user} , { status: 200 });
    
    } catch (error) {
