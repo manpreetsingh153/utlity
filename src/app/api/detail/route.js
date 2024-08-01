7099c7a72f655225ba9105cbb8d8f439034c7606
Manpreet singh <71442397+manpreetsingh153@users.noreply.github.com>
2024-08-01 13:17:37 +0530

    Update route.js

--- src/app/api/detail/route.js
+++ src/app/api/detail/route.js
@@ -1,1 +1,23 @@
 
+import { PrismaClient } from "@prisma/client";
+var mysql = require("mysql");
+ 
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+
+  try {
+
+
+     return NextResponse.json({data: 'userExist'} , { status: 200 });
+   
+   } catch (error) {
+        return NextResponse.json(
+           {
+              message: error,
+           },
+           { status: 500 }
+        );
+     }
+  
+}
